f8a3fb719c1c9346c7fb557e4a869c7a2cabbb4b
Amine Saber <saberamine000@gmail.com>
2023-04-17 16:57:35 -0700

    fixed error

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,12 +1,6 @@
-/**
- * @file 0-object_like_macro.h
- *
- * @author Amine Saber
- *
- * description : macro named size equal to token 1024
- *
- * @date 2023-04-17
- *
- */
+#ifndef _OBJECT_LIKE_MACRO_H
+#define _OBJECT_LIKE_MACRO_H
 
 #define SIZE 1024
+
+#endi
