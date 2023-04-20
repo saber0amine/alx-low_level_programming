c82c0cb1f7a9fd93c6b1ce08ed4664333ee07cbc
Amine Saber <saberamine000@gmail.com>
2023-04-19 19:34:45 -0700

    finally 🎶🎶🤩 project completed

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,8 +1,19 @@
-#ifndef _FUNCTION_NAME_H
-#define _FUNCTION_NAME_H
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
 
 void print_name(char *name, void (*f)(char *));
-int int_index(int *array, int size, int (*cmp)(int));
 void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
 
-#endif
+/**
+ * enum bool - typedef function for bool
+ * @TRUE: 1
+ * @FALSE: 0
+*/
+typedef enum bool
+{
+	TRUE = 1,
+	FALSE = 0
+} bool;
+
+#endif /* FUNCTION_POINTERS_H */
