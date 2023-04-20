011f72c248b205b27519626351381e1aa589dc47
Amine Saber <saberamine000@gmail.com>
2023-04-19 18:28:48 -0700

    task 0 fixed problem

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,10 +1,10 @@
 /**
- *edescription:  function to print name
+ * print_name - function to print name
  *
  * @name: name to print
  * @f: function pointer that doesn't return anything
  *
- * Return: nothing
+ * Return: Always 0 (Success)
 */
 
 void print_name(char *name, void (*f)(char *))
