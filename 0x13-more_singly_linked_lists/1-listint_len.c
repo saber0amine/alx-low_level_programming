a8817a01fc3673079b17dfe45b84c15fc05b61e6
Amine Saber <saberamine000@gmail.com>
2023-04-29 15:45:01 -0700

    solution of task 1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * print_listint -parcour of list
+ * @h: The list to print
+ * Return: The number of nodes
+ */
+size_t listint_len(const listint_t *h)
+{
+size_t count = 0;
+while (h != NULL)
+{
+h = h->next;
+count++;
+}
+return (count);
+}
+
