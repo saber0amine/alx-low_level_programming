d503f74c0e9c326219f2bc0ee86e5c56756ad584
Amine Saber <saberamine000@gmail.com>
2023-04-30 10:14:16 -0700

    fix errors 👌

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -3,28 +3,16 @@
 /**
  * free_listint - function that frees a listint_t list.
  * @tmp:a current node to track all the list.
- * @q:a node that pont to current node for free.
  * @head: header of the list.
  */
-
-
 
 void free_listint(listint_t *head)
 {
 listint_t *tmp = head;
-listint_t *q = head;
-
-if (head == NULL)
+while (tmp != NULL)
 {
+head = head->next;
 free(tmp);
-}
-else
-{
-while (tmp->next != NULL)
-{
-tmp = q;
-tmp = tmp->next;
-free(q);
-}
+tmp = head;
 }
 }
