0c898a1ef051490ad04fbf1d4cc1f5ccd2d2ffc7
Your Name <you@example.com>
2023-07-07 03:00:04 +0100

    add all files

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,46 +1,36 @@
-#include "main.h"
-/**
- * _stoi - converts chars to ints
- * @c: char to convert
- * Return: converted int
+/*
+ * Auth: Amine Saber 
+ * File: 0-binary_to_uint.c
  */
-unsigned int _stoi(char c)
-{
-	return ((unsigned int) c - '0');
-}
-/**
- * _strlen - calculates the length of the string
- * @s: input
- * Return: length of string
- */
-unsigned int _strlen(const char *s)
-{
-	unsigned int i;
 
-	for (i = 0; s[i]; i++)
-		;
-	return (i);
-}
+#include "main.h"
+
 /**
- * binary_to_uint - converts a string of 1's and 0's to a decimal number
- * @b: string to convert
- * Return: unsigned decimal number
+ * binary_to_uint - Converts a binary number to an unsigned int.
+ * @b: A pointer to a string of 0 and 1 chars.
+ *
+ * Return: If b is NULL or contains chars not 0 or 1 - 0.
+ *         Otherwise - the converted number.
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int i;
-	unsigned int result, tmp, expo;
+	unsigned int num = 0, mult = 1;
+	int len;
 
-	if (!b)
+	if (b == '\0')
 		return (0);
-	result = tmp = 0;
-	expo = 1;
-	for (i = _strlen(b) - 1; b[i]; i--, expo *= 2)
+
+	for (len = 0; b[len];)
+		len++;
+
+	for (len -= 1; len >= 0; len--)
 	{
-		if (b[i] != '0' && b[i] != '1')
+		if (b[len] != '0' && b[len] != '1')
 			return (0);
-		tmp = _stoi(b[i]);
-		result += tmp * expo;
+
+		num += (b[len] - '0') * mult;
+		mult *= 2;
 	}
-	return (result);
+
+	return (num);
 }
--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,18 +1,27 @@
+/*
+ * File: 5-flip_bits.c
+ * Auth: Amine Saber
+ */
+
 #include "main.h"
+
 /**
- * flip_bits - returns the number of bits you would need to flip
- * to get from one number to another
- * @n: base number
- * @m: number to transform to
- * Return: the number of bit transformations needed
+ * flip_bits - Counts the number of bits needed to be
+ *             flipped to get from one number to another.
+ * @n: The number.
+ * @m: The number to flip n to.
+ *
+ * Return: The necessary number of bits to flip to get from n to m.
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	int i, res;
+	unsigned long int xor = n ^ m, bits = 0;
 
-	res = 0;
-	for (i = 8 * sizeof(n) - 1; i >= 0; i--)
-		if (((n ^ m) >> i) & 1)
-			res++;
-	return (res);
+	while (xor > 0)
+	{
+		bits += (xor & 1);
+		xor >>= 1;
+	}
+
+	return (bits);
 }
