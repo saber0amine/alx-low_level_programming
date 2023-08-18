ee374522863dadc5434a51b13a06a46728225e7d
Your Name <you@example.com>
2023-08-18 16:55:36 +0100

    add new tasks 😎

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,18 +1,1 @@
-#include "lists.h"
-
-/**
-  * sum_dlistint - returns the sum of all the data (n) of a double linked list
-  * @head: pointer to the head node
-  *
-  * Return: integer
-  */
-int sum_dlistint(dlistint_t *head)
-{
-	int sum = 0;
-
-	while (head)
-		sum += head->n, head = head->next;
-
-	return (sum);
-}
-
+906609
