1748e0f69b9b5102b1cecbf5d76234519ae9780e
saberamine0 <saberamine000@gmail.com>
2023-08-21 23:52:17 +0100

    FIX ERRORS

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
-gcc -fPIC -g -c -Wall -pedantic -Werror -Wextra *.c
-gcc -shared -Wl,-soname,liball.so -o liball.so *.o -lc
+
+# Get all the .c files in the current directory
+c_files=$(ls *.c 2>/dev/null)
+
+# Check if any .c files exist
+if [ -z "$c_files" ]; then
+  echo "No .c files found in the current directory."
+  exit 1
+fi
+
+# Compile and create the dynamic library
+gcc -shared -fPIC -o liball.so $c_files
+
+# Check if the compilation was successful
+if [ $? -eq 0 ]; then
+  echo "Dynamic library liball.so created successfully."
+else
+  echo "Failed to create the dynamic library."
+fi
+
