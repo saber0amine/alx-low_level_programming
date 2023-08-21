a9c5569f3f6c9fcee954fcaf10a07213069a8f2d
saberamine0 <saberamine000@gmail.com>
2023-08-22 00:01:29 +0100

    FIX

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-wget -P .. https://raw.githubusercontent.com/Official0mega/alx-low_level_programming/0x18-dynamic_libraries/libgiga.so
-export LD_PRELOAD="$PWD/../libgiga.so"
+wget -P  /tmp https://github.com/emmylem/alx-low_level_programming
+/raw/master/0x18-dynamic_libraries/nrandom.so
+export LD_PRELOAD=/tmp/nrandom.so
